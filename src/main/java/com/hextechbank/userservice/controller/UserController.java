ab3bb19c97e304ea2775003ee6dce09ec24d28b6
Gabriel Miranda <qwerlolgabriel@gmail.com>
2025-09-29 20:06:04 -0300

    feat: add PUT endpoint to update user

--- src/main/java/com/hextechbank/userservice/controller/UserController.java
+++ src/main/java/com/hextechbank/userservice/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,4 +44,25 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails){
+        Optional<User> userOptional = userRepository.findById(id);
+        
+        if (userOptional.isPresent()){
+            User existingUser = userOptional.get();
+            existingUser.setFirstName(userDetails.getFirstName());
+            existingUser.setLastName(userDetails.getLastName());
+            existingUser.setEmail(userDetails.getEmail());
+            existingUser.setDocument(userDetails.getDocument());
+            existingUser.setPassword(userDetails.getPassword());
+        
+            User updateUser = userRepository.save(existingUser);
+            return ResponseEntity.ok(updateUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    
+    }
+    
 }
